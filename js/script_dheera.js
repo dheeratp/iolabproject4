b7196442754980c994c2d9d7ffc280585928813c
Dheera <dheera@ischool.berkeley.edu>
2013-12-03 01:46:57 -0800

    Code to draw circles for each of the 238 metros on the map

--- js/script_dheera.js
+++ js/script_dheera.js
@@ -1,121 +1,47 @@
-var metrosFile = 'json/metros.json';
-var metros=[];
-var marker;
-// geocoder= new google.maps.Geocoder();
+var latlngFile = 'json/metros_latlng.json';
+var ajaxConnections = 1;
+var citymap = {};
+var cityCircle;
 
-google.load('visualization', '1', {'packages': ['geochart','corechart']});
-
-$(document).ready(function()
-{
-  console.log("In ready");
-
-    getData();
-    google.setOnLoadCallback(drawMarkersMap);
-
-     console.log("In ready 1");
-
-
-    
-
+$(document).ready(function() {
+  $.getJSON(latlngFile, function (datainner) {
+    console.log("Inside latlng json read");
+    $.each(datainner.metros.metro,function(key,valueinner){
+      console.log("City "+valueinner.name+" Latitude "+valueinner.Latitude+" Longitude "+valueinner.Longitude);
+      citymap[valueinner.name]={center:new google.maps.LatLng(valueinner.Latitude, valueinner.Longitude),population:100};
+    });
+    initialize();
+  }).fail(function() {
+    console.log("FIRST fail");
+  });
 });
 
-
-function getData(){
- 
-    var ajaxConnections = 1;
-
-    console.log("In getData");
-    
-    var location=[];
-    var locationcoord;
-
-    $.getJSON(metrosFile, function (data) { 
-          ajaxConnections--;
-
-          if(ajaxConnections == 0) {
-            // all 3 have been retrieved
-            $.each(data.metros.metro,function(key,value){
-              location=[];
-
-                location.push(value.name);
-                metros.push(location);
-               });
-
-            //with the metros list, draw markers on the map
-             drawMarkersMap();
-
-        }
-      })
-        .fail(function() {
-          console.log("getmetros fail");
-          --ajaxConnections;
-        });
-
-    }
-
-function drawMarkersMap() {
-        console.log("In draw");
-         
-        console.log("number of metros = "+metros.length);
-
-        var table = new google.visualization.DataTable();
-
-        table.addColumn('string','Metro');
-        var rows=metros;
-        table.addRows(rows);
-
-       /*   var data = google.visualization.arrayToDataTable([
-            ['City',   'Population', 'Area'],
-            ['Rome',      2761477,    1285.31],
-            ['Milan',     1324110,    181.76],
-            ['Naples',    959574,     117.27],
-            ['Turin',     907563,     130.17],
-            ['Palermo',   655875,     158.9],
-            ['Genoa',     607906,     243.60],
-            ['Bologna',   380181,     140.7],
-            ['Florence',  371282,     102.41],
-            ['Fiumicino', 67370,      213.44],
-            ['Anzio',     52192,      43.43],
-            ['Ciampino',  38262,      11]
-          ]);*/
-        
-
-          //begin: Code for marker
-/*
-            geocoder.geocode( { 'address': value.name}, function(results, status) 
-            {
-                            if(status==google.maps.GeocoderStatus.OK){
-
-                              locationcoord=results[0].geometry.location;
-                              //console.log("LATITUDE LONGITUDE="+locationcoord);
-                             
-                            }
-                            else{
-                                console.log("Geocode was not successful for the following reason: " + status);
-                            }
-            });
-
-
-
-              marker = new google.maps.Marker({
-                  draggable:true,
-                  animation: google.maps.Animation.DROP,
-                  position: locationcoord
-                });
-*/
-
-          //end: Code for marker
-
+function initialize() {
   
+  // Create the map.
+  var mapOptions = {
+    zoom: 4,
+    center: new google.maps.LatLng(37.09024, -95.712891),
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+  mapOptions);
+  // Construct the circle for each value in citymap.
+  // Note: We scale the population by a factor of 20.
+  for (var city in citymap) {
+    var populationOptions = {
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.5,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: map,
+      center: citymap[city].center,
+      radius: citymap[city].population*5000 / 20
+    };
+    // Add the circle for this city to the map.
+    cityCircle = new google.maps.Circle(populationOptions);
+  }
 
-          var options = {
-            //region: 'IT',
-            backgroundColor: '868D91',
-            displayMode: 'markers',
-            sizeAxis: {minSize: 2, maxSize: 2},
-            colorAxis: {minValue: 1, maxValue:3,  colors: ['#438094','#DE3403','#E0D39E']} 
-          };
-          //colorAxis: {minValue: 0,  colors: ['#FF0000', '#00FF00']}
-          var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
-          chart.draw(table, options);
-    }
+}
+
