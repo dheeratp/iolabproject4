1876b41cec8894ed2d910c4fc035c23b204f0776
Dheera <dheera@ischool.berkeley.edu>
2013-12-10 15:21:16 -0800

    Code changes for colored bubbles on map for top tracks

--- js/main-rr.js
+++ js/main-rr.js
@@ -19,10 +19,20 @@
     var TOPCHARTS =[];
     var METROS={};
 
+    //use these for displaying legend
+    var ALLTOPTRACKS = [];
+    var obj={};
+    var objtoptracks={};
+    var toptrackslegend=[];
+    var colors=["#BF2EBD","#8460A6","#017DD0","#0600BD","#57EBFF","#6B008F","#FF1100","#FF8FC7","#0E0907","#AD9600","#6C3337","#FF7C0A","#D5BDFF","#936CA3","#CAFA19","#F5FF85","#9AA76C"];
+    var latlngFile = 'json/metros_latlng.json';
+    var citymap = {};
+
     //deferred object: Indicates that all TOPTRACKS are processed
     var getDataDone = $.Deferred();
     //deferred object: Indicates that all Metros are processed
     var getMetroDataDone = $.Deferred();
+
 
 
 
@@ -33,12 +43,29 @@ $(window).load(function () {
     //TODO - Need to pass the artist and track on the click event in modal
     loadPlayer("Lorde", "Royals");
 
+    //start: added by dheera
+
+    //display map by gettin all metros for last.fm from metros.json file which is a cached JSON file with information about metros that last.fm operates in  
+      $.getJSON(latlngFile, function (datainner) {
+       // console.log("Inside latlng json read");
+        $.each(datainner.metros.metro,function(key,valueinner){
+          //console.log("City "+valueinner.name+" Latitude "+valueinner.Latitude+" Longitude "+valueinner.Longitude);
+          citymap[valueinner.name]={center:new google.maps.LatLng(valueinner.Latitude, valueinner.Longitude),population:100,Country: valueinner.Country};
+
+        });
+        initialize();
+      }).fail(function() {
+        console.log("FIRST fail");
+      });
+
+    //end: added by dheera
+
     getMetroData();
 
     //Deferred call to getData and getChartData
     $.when(getMetroDataDone).then(function(){
         console.log("Got Metro Data ");
-        getData();
+        //getData();
         getChartData()
 
     })
@@ -51,6 +78,102 @@ $(window).load(function () {
     
 
 });
+
+function initialize() {
+
+  // Create the map.
+  var mapOptions = {
+    zoom: 4,
+    center: new google.maps.LatLng(37.09024, -95.712891),
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+  mapOptions);
+
+  //get top track for each metro: {cityname:track}
+  
+  getData();
+
+  $.when(getDataDone).then(function(){
+          console.log("Tracks processing complete ALLTOPTRACKS.length="+ALLTOPTRACKS.length);
+          console.log("Integration Point - RenderData Fn here");
+          //TODO - Invoke renderData here
+            for(var i=0;i<ALLTOPTRACKS.length;i++)
+            {
+                toptrackslegend[i]=ALLTOPTRACKS[i]["toptrack"];
+            }
+          //sort the legend array
+          toptrackslegend=toptrackslegend.sort();
+          console.log("toptrackslegend = "+toptrackslegend);
+
+      
+          //code for getting unique tracks for displaying the legend
+
+            
+              var hash = {}, result = [];
+              for ( var i = 0, l = toptrackslegend.length; i < l; ++i ) {
+          
+                if ( !hash.hasOwnProperty(toptrackslegend[i]) ) { //it works with objects! in FF, at least
+              
+                  hash[ toptrackslegend[i] ] = true;
+                  result.push(toptrackslegend[i]);
+                }
+              }
+            console.log(" RESULT ="+ result)
+
+          //
+
+          //pick random colors from array to display as legend
+          var randcolor = result[Math.floor(Math.random() * result.length)];
+          for(var index=0;index<result.length;index++)
+          {
+            $("#legendlist").append('<li class="legendItem"><span class="trackLegendColor" style="background:'+colors[index]+';"></span><p class="trackLegendName">'+result[index]+'</p></li>');
+          
+          }
+
+      // Construct the circle for each value in citymap.
+      // Note: We scale the population by a factor of 20.
+        var metrocolor,trackindex;
+          for (var city in citymap) {
+
+            for (var iterate=0;iterate<ALLTOPTRACKS.length;iterate++)
+            {
+                if(ALLTOPTRACKS[iterate]["city"]==city)
+                {
+                  trackindex=result.indexOf(ALLTOPTRACKS[iterate]["toptrack"]);
+                  metrocolor=colors[trackindex];
+                  console.log("COLOR for metro"+city+" is"+metrocolor);
+                }
+               
+            }
+
+             var newCityCircle = {
+              path: 'M 100 100 L 300 100 L 200 300 z',
+              strokeColor: metrocolor,
+              strokeWeight:6,
+              scale:.01
+          }
+
+          var populationOptions = {
+            icon: newCityCircle,
+            map: map,
+            position: new google.maps.LatLng(citymap[city]['center']['nb'],citymap[city]['center']['ob'] ),
+          };
+
+          marker = new google.maps.Marker(populationOptions); //{
+
+          // On click
+          google.maps.event.addListener(marker, 'click', function() {
+
+          window.alert("this div");
+          });
+
+          }
+
+      });
+
+}
+
 
 /*----getMetroData-----*/
 function getMetroData(){
@@ -66,9 +189,9 @@ var handleMetroDatavar=function getMetros(){
     },
     {
         success: function(data) {
-            console.log("getmetros");
+            //console.log("getmetros");
             $.extend(METROS, data );
-            console.log(METROS);
+            //console.log(METROS);
             getMetroDataDone.resolve(); 
             
         },
@@ -81,19 +204,64 @@ var handleMetroDatavar=function getMetros(){
 
 /*Assembles the Data for top tracks */
 function getData(){
-    $.ajax({
-        success:handleDatavar
- })
+   console.log("citymap.length="+citymap.length);
+    for (var city in citymap) {
+      //console.log("getting top track for the country "+citymap[city].Country+" corresponding to the metro "+ city);
+        
+        getTopTracks(citymap[city].Country, city);
+    }
+    console.log("HELLO ALLTOPTRACKS.length=" +ALLTOPTRACKS.length);
 }
 
-var handleDatavar=function handleData() {
-    var metro=METROS.metros.metro;
-    $.each(metro, function(i, object) {
-        console.log()
-    getTopTracks(object.country, object.name);
+function getTopTracks(country,city){
+//dheera start
+var url="http://ws.audioscrobbler.com/2.0/?method=geo.getTopTracks&api_key="+apiKey+"&country="+country+"&city="+city+"&format=json";
+console.log(url);
+$.getJSON(url,function(data) {
+  var index=0;
+    $.each(data, function(key, value) {
+     
+    
+        if(index==237)
+        {
+          console.log("reached 237");
+        }
+      else{
+            //var jsonData = JSON.parse(JSON.stringify(data.toptracks.track));
+            //console.log("city="+city+ " country="+country+" top track="+data.toptracks.track[0]["name"]);
+            
+            objtoptracks["city"]=city;
+            objtoptracks["data"]=data;
+            //TOPTRACKS[index].toptracks['@attr']['metro'] =city;
+           TOPTRACKS.push(objtoptracks);
+
+            obj["city"]=city;
+            obj["country"]=country;
+            obj["toptrack"]=data.toptracks.track[0]["name"];
+            
+
+            
+            ALLTOPTRACKS.push(obj);
+
+            obj = {};
+            objtoptracks = {};
+          }
+
     });
-}
+    if(ALLTOPTRACKS.length==237&&TOPTRACKS.length==237){
+        console.log("array length "+ALLTOPTRACKS.length);
+       getDataDone.resolve(); 
 
+      }
+
+
+  });
+//dheera end
+
+}
+/* End of code for getting top tracks for each metro */
+
+/*
 function getTopTracks(country, metro){
     lastfm.geo.getTopTracks
     ({
@@ -104,9 +272,24 @@ function getTopTracks(country, metro){
         success: function(data) {
             TOPTRACKS.push(data);
             console.log(TOPTRACKS); 
-            /*if (TOPTRACKS.length==METROS.metros.metro.length){
-                getDataDone.resolve(); 
-            }*/
+
+          //start: added by dheera
+            obj["city"]=metro;
+            obj["country"]=country;
+            obj["toptrack"]=data.toptracks.track[0]["name"];
+            
+            ALLTOPTRACKS.push(obj);
+
+            obj = {};
+
+        if(ALLTOPTRACKS.length==237){
+          console.log("array length "+ALLTOPTRACKS.length);
+           getDataDone.resolve(); 
+
+          }
+
+        //end: added by dheera
+
         },
         error: function(data) {
             console.log("getTopTracks: " + data.error + " " + data.message);
@@ -134,8 +317,8 @@ var renderTopTracksVar = function renderTopTracks(){
             }
             );
 
-           /*$('#top_tracks').html( $('#lastfmTemplateTracks').render(data.track[i]));
-            // do something*/
+           //$('#top_tracks').html( $('#lastfmTemplateTracks').render(data.track[i]));
+            // do something
         },
         error: function() 
         {
@@ -143,6 +326,7 @@ var renderTopTracksVar = function renderTopTracks(){
         }
     });
 }
+*/
 
 /*Assembles the Data for top charts */
 function getChartData(){
@@ -171,8 +355,8 @@ function getMetroTrackCharts(country, city){
     {
         success: function(data) {
             TOPCHARTS.push(data);
-            console.log("top charts"); 
-            console.log(TOPCHARTS);
+            //console.log("top charts"); 
+            //console.log(TOPCHARTS);
             
         },
         error: function(data) {
